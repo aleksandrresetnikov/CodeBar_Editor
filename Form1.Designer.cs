6ea93acd852484fe1ae8ef7a733537b1c3d3f255
aleksandrresetnikov <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-04-08 15:29:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace BARCODE_CREATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CODEBAR",
+            "CODE_39",
+            "CODE_93",
+            "CODE_128",
+            "QR_CODE",
+            "MSI",
+            "DATA_MATRIX"});
+            this.comboBox1.Location = new System.Drawing.Point(169, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "CODEBAR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Загрузить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 202);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ZXing;
+using ZXing.Rendering;
+using ZXing.QrCode;
+using ZXing.Common;
+
+namespace BARCODE_CREATOR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDown;
+        }
+
+        public static Image CreateCode(string text, int w, int h, BarcodeFormat format)
+        {
+            // Метод для создания кода
+
+            try
+            {
+                // Создаём экземпляр класса BarcodeWriter для зашифровки теста
+
+                BarcodeWriter writer = new BarcodeWriter
+                {
+                    Format = format,
+                    Options = new QrCodeEncodingOptions
+                    {
+                        Width = w,
+                        Height = h,
+                        CharacterSet = "UTF-8"
+                    },
+                    Renderer = new BitmapRenderer()
+                };
+
+                // Зашифровываем текст в необходимый графический код.
+                return writer.Write(text);
+            }
+            catch (Exception ex) { }
+
+            return null;
+        }
+
+        public static string[] CodeScan(Bitmap bmp)
+        {
+            // Метод для расшифровки изоображения (автоматически опознаёт нужный код)
+
+            try
+            {
+                BarcodeReader reader = new BarcodeReader
+                {
+                    AutoRotate = true,
+                    TryInverted = true,
+                    Options = new DecodingOptions
+                    {
+                        TryHarder = true
+                    }
+                };
+
+                Result[] results = reader.DecodeMultiple(bmp);
+
+                if (results != null)
+                    return results.Where(x => x != null && !string.IsNullOrEmpty(x.Text)).Select(x => x.Text).ToArray();
+            }
+            catch (Exception) { }
+
+            return null;
+        }
+
+        public static string DecodeImage(Image img)
+        {
+            // Дукодирует код с изображения
+
+            string outString = "";
+
+            string[] results = CodeScan((Bitmap)img);
+
+            if (results != null)
+                outString = string.Join(Environment.NewLine + Environment.NewLine, results);
+
+            return outString;
+        }
+
+        private BarcodeFormat GetFormat()
+        {
+            // Возвращает выбранный формат кода
+
+            switch (comboBox1.Text)
+            {
+                case "CODEBAR": return BarcodeFormat.CODABAR;
+                case "CODE_39": return BarcodeFormat.CODE_39;
+                case "CODE_93": return BarcodeFormat.CODE_93;
+                case "CODE_128": return BarcodeFormat.CODE_128;
+                case "QR_CODE": return BarcodeFormat.QR_CODE;
+                case "MSI": return BarcodeFormat.MSI;
+                case "DATA_MATRIX": return BarcodeFormat.DATA_MATRIX;
+                default: return BarcodeFormat.CODABAR;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialog = openFileDialog1.ShowDialog();
+
+                if (dialog == DialogResult.OK)
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+            catch (Exception ex) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(DecodeImage(pictureBox1.Image));
+            }
+            catch (Exception ex) { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.FileName = "img.png";
+
+                DialogResult dialog = saveFileDialog1.ShowDialog();
+
+                if (dialog == DialogResult.OK)
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+            catch (Exception ex) { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = CreateCode(textBox1.Text, pictureBox1.Width, pictureBox1.Height, GetFormat());
+        }
+    }
+}
